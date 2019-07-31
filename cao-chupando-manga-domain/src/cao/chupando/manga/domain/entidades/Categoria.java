9f667f79e635e576b78c3fd543a4b51092892827
Thi69 <thiago.ruiz.trl@gmail.com>
2019-07-31 19:42:14 -0400

    Adiciona a entidade categoria

---
+++ cao-chupando-manga-domain/src/cao/chupando/manga/domain/entidades/Categoria.java
@@ -0,0 +1,24 @@
+package cao.chupando.manga.domain.entidades;
+
+public class Categoria {
+    private int id;
+    private String nome;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    
+}
