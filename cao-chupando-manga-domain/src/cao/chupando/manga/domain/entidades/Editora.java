cb012991973036fc59017eaeddfb8577f1cce456
Thi69 <thiago.ruiz.trl@gmail.com>
2019-07-31 19:54:52 -0400

    Adiciona a entidade Editora

---
+++ cao-chupando-manga-domain/src/cao/chupando/manga/domain/entidades/Editora.java
@@ -0,0 +1,33 @@
+package cao.chupando.manga.domain.entidades;
+
+public class Editora {
+    private int Id;
+    private String nome;
+    private String nacionalidade;
+
+    public int getId() {
+        return Id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+    
+    
+}
