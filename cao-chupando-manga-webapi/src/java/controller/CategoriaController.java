74498ddbe29c13c2c0b251bb6726bad032a6b552
Thi69 <thiago.ruiz.trl@gmail.com>
2019-08-14 19:23:32 -0400

    Adicionando

--- cao-chupando-manga-webapi/src/java/controller/CategoriaController.java
+++ cao-chupando-manga-webapi/src/java/controller/CategoriaController.java
@@ -1,18 +1,48 @@
 package controller;
 
+import cao.chupando.manga.domain.dao.ICategoriaDAO;
+import cao.chupando.manga.domain.dao.list.CategoriaDAOimpl;
+import cao.chupando.manga.domain.entidades.Categoria;
+import java.util.List;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("/categoria") 
 public class CategoriaController {
     
+    private ICategoriaDAO banco = new CategoriaDAOimpl();
     
     @GET
     @Path("/")
+    @Produces(MediaType.APPLICATION_JSON) 
+    public List<Categoria> index(){
+        return banco.consultar();
+    }
+
+    @GET
+    @Path("/select/{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public String index(){
-        return "teste";
+    public Categoria select(@PathParam("id") int pk){
+        System.out.println("Parametro:"+pk);
+        for(Categoria cat : banco.consultar()){
+            if(cat.getId() == pk){
+                return cat;
+            }
+        }
+        return null;
+    }
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/cadastrar/{nome}")
+    public String cadastrar(@PathParam("nome")String nome){
+        Categoria nova = new Categoria();
+        nova.setNome(nome);
+        banco.inserir(nova);
+        String ret = "{\"status\": 1}";
+        return ret;
     }
 }
